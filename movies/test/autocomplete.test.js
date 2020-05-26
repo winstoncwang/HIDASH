6454ace41b90961321f57eb89413d4139df3e5f5
winstoncwang <51161138+winstoncwang@users.noreply.github.com>
2020-05-26 12:50:26 +0100

    1.6
    add beforeEach hook that initialises dropdown menu, add helper function to wait for the result before checking dropdown display

--- movies/test/autocomplete.test.js
+++ movies/test/autocomplete.test.js
@@ -1,4 +1,50 @@
-it('Shows an autocomplete', () => {
+//helper
+const waitFor = (selector) => {
+	return new Promise((resolve, reject) => {
+		//setIntervale to check
+		const interval = setInterval(() => {
+			if (document.querySelector(selector)) {
+				clearInterval(interval);
+				clearTimeout(timeout);
+				resolve();
+			}
+		}, 30);
+
+		const timeout = setTimeout(() => {
+			clearInterval(interval);
+			reject();
+		}, 2000);
+	});
+};
+
+//initiate the testing environment
+beforeEach(() => {
+	//empty search bar
+	document.querySelector('#target').innerHTML = '';
+	//any value we type should show a list of results
+	createAutoComplete({
+		root         : document.querySelector('#target'),
+		fetchData () {
+			return [
+				{ Title: 'Avengers' },
+				{ Title: 'Tom and Jerry' },
+				{ Title: 'Bob Builder' },
+				{ Title: 'Tommy' }
+			];
+		},
+		renderOption (movie) {
+			return `${movie.Title}`;
+		}
+	});
+
+	//testing dropdown component should be defaulted to no showing
+	//using chai.js from html, assertion module similar to node's
+
+	const dropdown = document.querySelector('.dropdown');
+	assert.notInclude(dropdown.className, 'is-active');
+});
+
+it('Dropdown starts closed', () => {
 	//any value we type should show a list of results
 
 	createAutoComplete({
@@ -21,4 +67,19 @@ it('Shows an autocomplete', () => {
 
 	const dropdown = document.querySelector('.dropdown');
 	assert.notInclude(dropdown.className, 'is-active');
+});
+
+it('After searching, dropdown opens up', async () => {
+	const input = document.querySelector('input');
+	input.value = 'avengers';
+	input.dispatchEvent(new Event('input'));
+
+	//wait for content then check for opening up of dropdown
+	//helper function
+	await waitFor('.dropdown-item');
+
+	//because of debounce used for input value, search result is not instant
+	//hence we need to hold assertion
+	const dropdown = document.querySelector('.dropdown');
+	assert.include(dropdown.className, 'is-active');
 });
